e5a9a8f07ea8343f00a72575e04b51e54c4e1a04
vidyaaranyaM <vidyaaranyamacha@gmail.com>
2023-09-25 10:21:48 -0700

    added a get_response function in chat.py

--- chat.py
+++ chat.py
@@ -40,6 +40,26 @@ class ChatBot:
         self.outputSize = data['outputSize']
         self.allWords = data['allWords']
         self.tags = data['tags']
+    
+    def get_response(self, sentence):
+        sentence = self.utils.tokenize(sentence)
+        X = self.utils.bagOfWords(sentence, self.allWords)
+        X = X.reshape(1, X.shape[0])
+        X = torch.from_numpy(X)
+
+        output = self.model(X)
+        _, predicted = torch.max(output, dim=1)
+
+        tag = self.tags[predicted.item()]
+
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][predicted.item()]
+        if prob.item() > 0.75:
+            for intent in self.intents['intents']:
+                if tag == intent["tag"]:
+                    print(f"{self.botName}: {random.choice(intent['responses'])}")
+        else:
+            print(f"{self.botName}: I do not understand...")
 
     def chat(self):
         print("Let's chat! (type 'quit' to exit)")
@@ -48,24 +68,7 @@ class ChatBot:
             if sentence == "quit":
                 break
 
-            sentence = self.utils.tokenize(sentence)
-            X = self.utils.bagOfWords(sentence, self.allWords)
-            X = X.reshape(1, X.shape[0])
-            X = torch.from_numpy(X)
-
-            output = self.model(X)
-            _, predicted = torch.max(output, dim=1)
-
-            tag = self.tags[predicted.item()]
-
-            probs = torch.softmax(output, dim=1)
-            prob = probs[0][predicted.item()]
-            if prob.item() > 0.75:
-                for intent in self.intents['intents']:
-                    if tag == intent["tag"]:
-                        print(f"{self.botName}: {random.choice(intent['responses'])}")
-            else:
-                print(f"{self.botName}: I do not understand...")
+            self.get_response(sentence)
 
 def main(_):
     bot = ChatBot()
